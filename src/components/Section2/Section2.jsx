03c159dcf8d56f9f8e16e42b42e0ef58376d4863
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:06:44 -0400

    add components/Section2

---
+++ src/components/Section2/Section2.jsx
@@ -0,0 +1,11 @@
+import style from './Section2.module.css'
+
+function Section2 () {
+  return (
+    <section className={style.Section2}>
+      LAS FINALIDADES DE LA TRATA DE PERSONAS
+    </section>
+  )
+}
+
+export default Section2
