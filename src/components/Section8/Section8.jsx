63e07d5e113a9adb9440bed31be826b79a457057
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:07:36 -0400

    add components/Section8

---
+++ src/components/Section8/Section8.jsx
@@ -0,0 +1,11 @@
+import style from './Section8.module.css'
+
+function Section8 () {
+  return (
+    <section className={style.Section8}>
+      CRÉDITOS
+    </section>
+  )
+}
+
+export default Section8
