15cbb823a0ecdd66f6c9ce96a2510fd8f1f5a304
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:14:24 -0400

    add components/Section0

---
+++ src/components/Section0/Section0.jsx
@@ -0,0 +1,11 @@
+import style from './Section0.module.css'
+
+function Section0 () {
+  return (
+    <section className={style.Section0}>
+      TRATA DE PERSONAS
+    </section>
+  )
+}
+
+export default Section0
