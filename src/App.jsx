92d2370a9f59e1fa47864ba8392ca77483230d85
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:10:11 -0400

    change src/App.jsx: import new components (line 4-12) and add them to App (line 28-36)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 
 import Instructions from './components/Instructions/Instructions'
+import Section1 from './components/Section1/Section1'
+import Section2 from './components/Section2/Section2'
+import Section3 from './components/Section3/Section3'
+import Section4 from './components/Section4/Section4'
+import Section5 from './components/Section5/Section5'
+import Section6 from './components/Section6/Section6'
+import Section7 from './components/Section7/Section7'
+import Section8 from './components/Section8/Section8'
+import Section9 from './components/Section9/Section9'
 
 import './App.css'
 
@@ -16,7 +25,15 @@ function App () {
         : null
     }
       <main>
-        contenido principal
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Section6 />
+        <Section7 />
+        <Section8 />
+        <Section9 />
       </main>
     </>
   )
