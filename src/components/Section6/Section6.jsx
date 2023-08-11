a1238d4fc6fc6366257445094cbbac8533ecebf7
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 10:41:21 -0400

    change components/Section6: import and use components: BarGraph, NoteBook, News, BigInfoBox (line 1-5, 12-15)

--- src/components/Section6/Section6.jsx
+++ src/components/Section6/Section6.jsx
@@ -1,9 +1,18 @@
+import NoteBook from '../NoteBook/NoteBook'
+import BarGraph from '../BarGraph/BarGraph'
+import News from '../News/News'
+import BigInfoBox from '../BigInfoBox/BigInfoBox'
+
 import style from './Section6.module.css'
 
 function Section6 () {
   return (
     <section className={style.Section6}>
       PREVENCIÓN
+      <NoteBook />
+      <BarGraph />
+      <BigInfoBox />
+      <News />
     </section>
   )
 }
