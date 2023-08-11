3362a5ca7485f0a7f31bed9cd097419b188562a5
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:07:47 -0400

    add components/Section9

---
+++ src/components/Section9/Section9.jsx
@@ -0,0 +1,11 @@
+import style from './Section9.module.css'
+
+function Section9 () {
+  return (
+    <section className={style.Section9}>
+      REFERENCIAS
+    </section>
+  )
+}
+
+export default Section9
