27d92dbd7c1802f7f2a8d82c536e5c9c01cd117d
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:07:04 -0400

    add components/Section4

---
+++ src/components/Section4/Section4.jsx
@@ -0,0 +1,11 @@
+import style from './Section4.module.css'
+
+function Section4 () {
+  return (
+    <section className={style.Section4}>
+      PROCURACIÓN DE LA JUSTICIA
+    </section>
+  )
+}
+
+export default Section4
