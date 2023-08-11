6acd6ff435a6c290f898f32e2cea29c62bf57a7f
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:55:44 -0400

    add components/SmallInfoBox

---
+++ src/components/SmallInfoBox/SmallInfoBox.jsx
@@ -0,0 +1,11 @@
+import style from './SmallInfoBox.module.css'
+
+function SmallInfoBox () {
+  return (
+    <div className={style.SmallInfoBox}>
+      SmallInfoBox
+    </div>
+  )
+}
+
+export default SmallInfoBox
