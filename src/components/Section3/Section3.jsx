fe363b062d243695be1ab26ed88fb5e051723be7
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 10:38:17 -0400

    change components/Section3: import and use components: NoteBook, BarGraph, SmallInfoBox, News (line 1-5, 12-15)

--- src/components/Section3/Section3.jsx
+++ src/components/Section3/Section3.jsx
@@ -1,9 +1,18 @@
+import NoteBook from '../NoteBook/NoteBook'
+import BarGraph from '../BarGraph/BarGraph'
+import SmallInfoBox from '../SmallInfoBox/SmallInfoBox'
+import News from '../News/News'
+
 import style from './Section3.module.css'
 
 function Section3 () {
   return (
     <section className={style.Section3}>
       PROTECCIÓN Y ATENCIÓN A LAS VÍCTIMAS
+      <NoteBook />
+      <BarGraph />
+      <SmallInfoBox />
+      <News />
     </section>
   )
 }
