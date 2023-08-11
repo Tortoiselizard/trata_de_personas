31c55270baf6416110047898b799f7158e2f5873
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:06:53 -0400

    add components/Section3

---
+++ src/components/Section3/Section3.jsx
@@ -0,0 +1,11 @@
+import style from './Section3.module.css'
+
+function Section3 () {
+  return (
+    <section className={style.Section3}>
+      PROTECCIÓN Y ATENCIÓN A LAS VÍCTIMAS
+    </section>
+  )
+}
+
+export default Section3
