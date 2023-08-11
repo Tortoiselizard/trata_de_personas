76380416891a8aedd147ec732a3d8f43b3b6696b
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:06:30 -0400

    add components/Section1

---
+++ src/components/Section1/Section1.jsx
@@ -0,0 +1,11 @@
+import style from './Section1.module.css'
+
+function Section1 () {
+  return (
+    <section className={style.Section1}>
+      VISIBILIZACIÓN DE LAS VÍCTIMAS
+    </section>
+  )
+}
+
+export default Section1
