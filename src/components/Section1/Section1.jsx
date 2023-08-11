d1c5486e42824b802b60af1f752c97e6446ed496
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 10:36:16 -0400

    change components/Section1: import and use components: Slides, NoteBook, News, Map, BigInfoBox, BarGraph (line 1-7, 14-20)

--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -1,9 +1,23 @@
+import Slides from '../Slides/Slides'
+import NoteBook from '../NoteBook/NoteBook'
+import BarGraph from '../BarGraph/BarGraph'
+import Map from '../Map/Map'
+import BigInfoBox from '../BigInfoBox/BigInfoBox'
+import News from '../News/News'
+
 import style from './Section1.module.css'
 
 function Section1 () {
   return (
     <section className={style.Section1}>
       VISIBILIZACIÓN DE LAS VÍCTIMAS
+      <Slides />
+      <NoteBook />
+      <BarGraph />
+      <Map />
+      <BigInfoBox />
+      <News />
+      <BigInfoBox />
     </section>
   )
 }
