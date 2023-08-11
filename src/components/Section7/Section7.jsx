e3c81686efa5b448db8d9ff0554a72030038d411
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:07:28 -0400

    add components/Section7

---
+++ src/components/Section7/Section7.jsx
@@ -0,0 +1,11 @@
+import style from './Section7.module.css'
+
+function Section7 () {
+  return (
+    <section className={style.Section7}>
+      ACONTECIMIENTOS DE INTERÉS
+    </section>
+  )
+}
+
+export default Section7
