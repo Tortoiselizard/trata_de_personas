509214c58a2c9049ea872d41dca390fca788bbe7
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 10:34:47 -0400

    change components/Section0: import and use components: NoteBook, News, Map, BigInfoBox (line 1-5, 12-15)

--- src/components/Section0/Section0.jsx
+++ src/components/Section0/Section0.jsx
@@ -1,9 +1,18 @@
+import NoteBook from '../NoteBook/NoteBook'
+import News from '../News/News'
+import Map from '../Map/Map'
+import BigInfoBox from '../BigInfoBox/BigInfoBox'
+
 import style from './Section0.module.css'
 
 function Section0 () {
   return (
     <section className={style.Section0}>
       TRATA DE PERSONAS
+      <NoteBook />
+      <News />
+      <Map />
+      <BigInfoBox />
     </section>
   )
 }
