d8e08fee15b1ec9ad63b4d12756c237b7cc4b37e
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 10:40:19 -0400

    change components/Section5: import and use components: BarGraph, NoteBook, Map, BigInfoBox (line 1-5, 12-15)

--- src/components/Section5/Section5.jsx
+++ src/components/Section5/Section5.jsx
@@ -1,9 +1,18 @@
+import NoteBook from '../NoteBook/NoteBook'
+import BarGraph from '../BarGraph/BarGraph'
+import Map from '../Map/Map'
+import BigInfoBox from '../BigInfoBox/BigInfoBox'
+
 import style from './Section5.module.css'
 
 function Section5 () {
   return (
     <section className={style.Section5}>
       REPATRIACIÓN
+      <NoteBook />
+      <BarGraph />
+      <Map />
+      <BigInfoBox />
     </section>
   )
 }
