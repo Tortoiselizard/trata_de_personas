e423aa1fdceef929ca8f82732dc165b0c15e660a
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:07:21 -0400

    add components/Section6

---
+++ src/components/Section6/Section6.jsx
@@ -0,0 +1,11 @@
+import style from './Section6.module.css'
+
+function Section6 () {
+  return (
+    <section className={style.Section6}>
+      PREVENCIÓN
+    </section>
+  )
+}
+
+export default Section6
