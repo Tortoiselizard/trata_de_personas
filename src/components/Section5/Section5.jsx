f2c6df4286fac0f609d8034c46f3678cc1c8f8a2
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:07:12 -0400

    add components/Section5

---
+++ src/components/Section5/Section5.jsx
@@ -0,0 +1,11 @@
+import style from './Section5.module.css'
+
+function Section5 () {
+  return (
+    <section className={style.Section5}>
+      REPATRIACIÓN
+    </section>
+  )
+}
+
+export default Section5
