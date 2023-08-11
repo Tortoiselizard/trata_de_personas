cb041768ec20a6eb47fb7ca797100cb660d9e265
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 10:37:21 -0400

    change components/Section2: import and use components: Slides, NoteBook, SmallInfoBox, BigInfoBox (line 1-5, 12-15)

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -1,9 +1,18 @@
+import Slides from '../Slides/Slides'
+import SmallInfoBox from '../SmallInfoBox/SmallInfoBox'
+import NoteBook from '../NoteBook/NoteBook'
+import BigInfoBox from '../BigInfoBox/BigInfoBox'
+
 import style from './Section2.module.css'
 
 function Section2 () {
   return (
     <section className={style.Section2}>
       LAS FINALIDADES DE LA TRATA DE PERSONAS
+      <Slides />
+      <SmallInfoBox />
+      <NoteBook />
+      <BigInfoBox />
     </section>
   )
 }
