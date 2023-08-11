0af82a5053e25f7cf0de73774e8e1e25ca8853da
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 08:50:36 -0400

    add components/BigInfoBox

---
+++ src/components/BigInfoBox/BigInfoBox.jsx
@@ -0,0 +1,11 @@
+import style from './BigInfoBox.module.css'
+
+function BigInfoBox () {
+  return (
+    <div className={style.BigInfoBox}>
+      BigInfoBox
+    </div>
+  )
+}
+
+export default BigInfoBox
