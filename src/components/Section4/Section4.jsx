6f938cf94a14f83e25fa50cf45025658b98552d1
Troy Pernía <troy00pernia@gmail.com>
2023-08-11 10:39:08 -0400

    change components/Section4: import and use components: BarGraph, SmallInfoBox, News (line 1-4, 11-13)

--- src/components/Section4/Section4.jsx
+++ src/components/Section4/Section4.jsx
@@ -1,9 +1,16 @@
+import BarGraph from '../BarGraph/BarGraph'
+import News from '../News/News'
+import SmallInfoBox from '../SmallInfoBox/SmallInfoBox'
+
 import style from './Section4.module.css'
 
 function Section4 () {
   return (
     <section className={style.Section4}>
       PROCURACIÓN DE LA JUSTICIA
+      <BarGraph />
+      <SmallInfoBox />
+      <News />
     </section>
   )
 }
